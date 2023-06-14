13c059a312ec4d048dab6e510d57633328099162
Markus Fleschutz <markus@fleschutz.de>
2023-06-14 15:46:54 +0200

    Update powershell-profile.ps1

--- powershell-profile.ps1
+++ powershell-profile.ps1
@@ -1,15 +1,13 @@
-﻿# MY POWERSHELL PROFILE (this profile file defines the look & feel of PowerShell)
-
-# WINDOW TITLE
+﻿# TERMINAL WINDOW TITLE
 if ($IsLinux) { $Username = $(whoami) } else { $Username = $env:USERNAME }
 $host.ui.RawUI.WindowTitle = "$Username @ $(hostname)"
 
 # COMMAND PROMPT
-function prompt { write-host -noNewline -foregroundColor yellow "`n➤"; return " " }
+function prompt { Write-Host -noNewline -foregroundColor yellow "`n➤"; return " " }
 
 # ALIAS NAMES
 del alias:pwd -force -errorAction SilentlyContinue
-set-alias -name pwd -value list-workdir.ps1	# pwd = print working directory
-set-alias -name ll -value get-childitem		# ll = list folder (long format)
+Set-Alias -name pwd -value list-workdir.ps1	# pwd = print working directory
+Set-Alias -name ll -value Get-ChildItem		# ll = list folder (long format)
 del alias:ls -force -errorAction SilentlyContinue 
-set-alias -name ls -value list-folder.ps1	# ls = list folder (short format)
+Set-Alias -name ls -value list-folder.ps1	# ls = list folder (short format)
