f63497db1fde9799e47205a2225a3760946f6505
Markus Fleschutz <markus.fleschutz@gmail.com>
2024-09-05 11:32:49 +0200

    Added .gitconfig

---
+++ my-powershell-profile.ps1
@@ -0,0 +1,19 @@
+﻿# MY POWERSHELL PROFILE (defines the look & feel of PowerShell)
+
+# WINDOW TITLE
+if ($IsLinux) { $username = $(whoami) } else { $username = $env:USERNAME }
+$host.ui.RawUI.WindowTitle = "$username @ $(hostname)"
+
+# GREETING
+Write-Host "👋 Hi $username @ $(hostname) - type 'hlp' for help." -foregroundColor green
+
+# COMMAND PROMPT
+function prompt { Write-Host "`n➤ " -noNewline -foregroundColor yellow; return " " }
+
+# ALIAS NAMES
+del alias:pwd -force -errorAction SilentlyContinue
+set-alias -name hlp -value write-help.ps1
+set-alias -name pwd -value list-workdir.ps1	# pwd = print working directory
+set-alias -name ll -value get-childitem		# ll = list folder (long format)
+del alias:ls -force -errorAction SilentlyContinue 
+set-alias -name ls -value list-folder.ps1	# ls = list folder (short format)
