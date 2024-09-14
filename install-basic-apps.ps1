fc4f51f943fcdae05bb2cf889784cd5f34016447
Markus Fleschutz <markus.fleschutz@gmail.com>
2024-09-14 17:13:02 +0200

    Updated install-basic-apps.ps1

--- install-basic-apps.ps1
+++ install-basic-apps.ps1
@@ -1,9 +1,20 @@
-# Install Basic Applications on Windows (sorted alphabetically)
-# =============================================================
+# Install Basic Apps on Windows (sorted alphabetically)
+# =====================================================
+& winget install 7zip.7zip
+& winget install CrystalDewWorld.CrystalDiskInfo
 & winget install Git.Git
 & winget install Google.Chrome
 & winget install Google.QuickShare
 & winget install Greenshot.Greenshot
+& winget install IrfanView64 --source msstore
+& winget install KDE.KDiff3
 & winget install Microsoft.VisualStudioCode
 & winget install Microsoft.WindowsTerminal
+& winget install Mozilla.Thunderbird
+& winget install Notepad++.Notepad++
+& winget install Prusa3D.PrusaSlicer
+& winget install smartmontools.smartmontools
+& winget install VideoLAN.VLC
+& winget install WhatsApp
+& winget install Zoom.Zoom.EXE
 exit 0 # success
