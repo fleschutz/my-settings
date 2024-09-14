8ce1356284266ff9b63ec9751e1f46a9860cac60
Markus Fleschutz <markus.fleschutz@gmail.com>
2024-09-14 16:55:44 +0200

    Update install-basic-apps.ps1

--- install-basic-apps.ps1
+++ install-basic-apps.ps1
@@ -1,4 +1,7 @@
+# Install Basic Applications on Windows (sorted alphabetically)
+# =============================================================
 & winget install Git.Git
 & winget install Google.Chrome
 & winget install Greenshot.Greenshot
+& winget install Microsoft.WindowsTerminal
 exit 0 # success
