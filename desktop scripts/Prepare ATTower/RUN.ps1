bc968939722ecbc63da9e0fe2858009d83fd511c
Markus Fleschutz <markus.fleschutz@atcsim.de>
2026-01-07 15:51:38 +0100

    Added some folder icons

---
+++ desktop scripts/Prepare ATTower/RUN.ps1
@@ -0,0 +1,18 @@
+﻿param([string]$path = "")
+
+try {
+        "Preparing and Clearing ATTower Installation"
+        "-------------------------------------------"
+        if ($path -eq "") { $path = Read-Host "Enter the path to ATTower" }
+        if (!(Test-Path "$path")) { throw "Invalid path" }
+
+        "(1) Copy SimControl configuration..."
+        Copy-Item "$PSScriptRoot/config.xml" -destination "$path/DBase/Profiles/SimControl" -force -passthru
+
+        "✅ ATTower at $path is prepared now."
+        Start-Sleep -milliseconds 5000
+        exit 0 # success
+} catch {
+        "⚠️ ERROR: $($Error[0]) (script line $($_.InvocationInfo.ScriptLineNumber))"
+        exit 1
+}
